985bb5b35b6b32d47cb522c833264888b785e51d
Rezhe <rezhewang@gmail.com>
2026-01-09 10:53:37 +1100

    did some changes

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import app_window
 import slint
 
 
-class App(app_window.AppWindow):
+# slint.loader will look in `sys.path` for `app-window.slint`.
+class App(slint.loader.app_window.AppWindow):
     @slint.callback
     def request_increase_value(self):
         self.counter = self.counter + 1
